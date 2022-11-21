9aed1c961665b4326e104a545c01cde008d69f68
Derek  Kweku Degbedzui <96236642+jrderek@users.noreply.github.com>
2022-11-21 18:40:22 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# friday
+
+
+****
+## Features
+1. Drap and drop file from local system for training.
+
+2. Simple Data Exploration.
+
+3. Many Preprocessing methods:
+     * Sample and Split
+     * Data Preprocessing (Missing Values Imputation, One Hot Encoding, Handle Unknown Levels, Fix Imbalance for Classification)
+     * Scale and Transform (Normalization, Transformation, Target Transformation)
+     * Feature Engineering (Feature Interaction, Polynomial Features, Trigonometry Features, Group Features, Bin Numeric Features, Combine Rare Levels)
+     * Feature Selection (Feature Importance, Remove Multicollinearity, Principal Components Analysis, Ignore Variances)
+     * Unsupervised (Create Clusters, Remove Outliers)
+
+4. Model Training:
+   * Compare all available Machine Learning Algorithm automatically.
+   * Train a selected single model
+   * Train an ensemble model
+   * Hyperparameter tuning for single model
+
+5. Model Result Visualization:
+   * All plots for Regression and Classification
+   * SHAP Value
+
+6. Prediction and Save Model:
+
+   * Online Prediction  
+   * Batch Prediction  
+   * Save whole Machine Learning Pipeline as pickle file
+
+****
+## Install and Run
+Drap and drop file from local system for training.
+
+Simple Data Exploration.
+
+Many Preprocessing methods:
+
+Sample and Split
+Data Preprocessing (Missing Values Imputation, One Hot Encoding, Handle Unknown Levels, Fix Imbalance for Classification)
+Scale and Transform (Normalization, Transformation, Target Transformation)
+Feature Engineering (Feature Interaction, Polynomial Features, Trigonometry Features, Group Features, Bin Numeric Features, Combine Rare Levels)
+Feature Selection (Feature Importance, Remove Multicollinearity, Principal Components Analysis, Ignore Variances)
+Unsupervised (Create Clusters, Remove Outliers)
+Model Training:
+
+Compare all available Machine Learning Algorithm automatically.
+Train a selected single model
+Train an ensemble model
+Hyperparameter tuning for single model
+Model Result Visualization:
+
+All plots for Regression and Classification
+SHAP Value
+Prediction and Save Model:
+
+Online Prediction
+Batch Prediction
+Save whole Machine Learning Pipeline as pickle file
+Install and Run
+Clone the repository to you computer:
+
+git clone https://github.com/jrderek/private.git
+cd private
+Creata a conda virtual or python virtual environment and then activate it.
+conda create -n myvirtual-name python=3.8 -y
+conda activate myvirtual-name
+Install requirements
+pip install -r requirements.txt
+Run streamlit locally and start web service:
+streamlit run app.py
+Run on Docker
+Pull the Docker image:
+docker pull jrdegbe/privately/github-action:0.1.0
+
+
+2. Run the  Docker image locally:
+docker run -p 8501:8501 privately/github-action
+
+3. Open the localhost:8501
+
+  * [http://localhost:8501](http://localhost:8501)
---
+++ streamlit/ui.py
@@ -0,0 +1,121 @@
+import io
+
+import requests
+from PIL import Image
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+import streamlit as st # web development
+import numpy as np # np mean, np random 
+import pandas as pd # read csv, df manipulation
+import time # to simulate a real time data, time loop 
+import plotly.express as px # interactive charts 
+
+
+# interact with FastAPI endpoint
+backend = "http://fastapi:8000/segmentation"
+
+
+def process(image, server_url: str):
+
+    m = MultipartEncoder(fields={"file": ("filename", image, "image/jpeg")})
+
+    r = requests.post(
+        server_url, data=m, headers={"Content-Type": m.content_type}, timeout=8000, verify=False
+    )
+
+    return r
+
+
+# construct UI layout
+st.set_page_config(
+    page_title = 'Test - Real-Time Data Science Dashboard',
+    page_icon = '✅',
+    layout = 'wide',
+    menu_items={'Get Help': 'https://www.extremelycoolapp.com/help',
+    'Report a bug': "https://www.extremelycoolapp.com/bug",
+    'About': "# This is a header. This is an *extremely* cool app! Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch."
+}
+)
+
+# dashboard title
+
+st.title("Real-Time Image Segmentor and Dashboard/ Live Data Science Dashboard")
+st.write(
+    """Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
+         This streamlit example uses a FastAPI service as backend.
+         Visit this URL at `:8000/docs` for FastAPI documentation."""
+)  # description and instructions
+
+input_image = st.file_uploader("insert image")  # image upload widget
+
+if st.button("Get segmentation map"):
+
+    col1, col2 = st.columns(2)
+
+    if input_image:
+        segments = process(input_image, backend)
+        original_image = Image.open(input_image).convert("RGB")
+        segmented_image = Image.open(io.BytesIO(segments.content)).convert("RGB")
+        col1.header("Original")
+        col1.image(original_image, use_column_width=True)
+        col2.header("Segmented")
+        col2.image(segmented_image, use_column_width=True)
+
+    else:
+        # handle case with no image
+        st.write("Do some cool AI and run the model - Insert an image!")
+
+# read csv
+df = pd.read_csv("./data/dashboard_data.csv")
+
+
+# top-level filters 
+
+job_filter = st.selectbox("Select the Job", pd.unique(df['job']))
+
+
+# creating a single-element container.
+placeholder = st.empty()
+
+# dataframe filter 
+
+df = df[df['job']==job_filter]
+
+# near real-time / live feed simulation 
+
+for seconds in range(200):
+#while True: 
+    
+    df['age_new'] = df['age'] * np.random.choice(range(1,5))
+    df['balance_new'] = df['balance'] * np.random.choice(range(1,5))
+
+    # creating KPIs 
+    avg_age = np.mean(df['age_new']) 
+
+    count_married = int(df[(df["marital"]=='married')]['marital'].count() + np.random.choice(range(1,30)))
+    
+    balance = np.mean(df['balance_new'])
+
+    with placeholder.container():
+        # create three columns
+        kpi1, kpi2, kpi3 = st.columns(3)
+
+        # fill in those three columns with respective metrics or KPIs 
+        kpi1.metric(label="Age ⏳", value=round(avg_age), delta= round(avg_age) - 10)
+        kpi2.metric(label="Married Count 💍", value= int(count_married), delta= - 10 + count_married)
+        kpi3.metric(label="A/C Balance ＄", value= f"$ {round(balance,2)} ", delta= - round(balance/count_married) * 100)
+
+        # create two columns for charts 
+
+        fig_col1, fig_col2 = st.columns(2)
+        with fig_col1:
+            st.markdown("### First Chart")
+            fig = px.density_heatmap(data_frame=df, y = 'age_new', x = 'marital')
+            st.write(fig)
+        with fig_col2:
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame = df, x = 'age_new')
+            st.write(fig2)
+        st.markdown("### Detailed Data View")
+        st.dataframe(df)
+        time.sleep(1)
+    #placeholder.empty()
